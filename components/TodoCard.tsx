517d0117a7a3ac893cbcabdba55b9fa95e930e12
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-06-05 09:49:38 +0100

    inner drage and drop

---
+++ components/TodoCard.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React from "react";
+import {
+  DraggableProvidedDragHandleProps,
+  DraggableProvidedDraggableProps,
+} from "react-beautiful-dnd";
+
+type Props = {
+  todo: Todo;
+  index: number;
+  id: TypedColumn;
+  innerRef: (element: HTMLElement | null) => void;
+  draggableProps: DraggableProvidedDraggableProps;
+  dragHandleProps: DraggableProvidedDragHandleProps | null | undefined;
+};
+
+const TodoCard = ({
+  id,
+  dragHandleProps,
+  draggableProps,
+  index,
+  innerRef,
+  todo,
+}: Props) => {
+  return <div className="bg-white rounded-md space-y-2 drop-shadow-md" {...draggableProps} {...dragHandleProps} ref={innerRef}>
+    <h1>Hello</h1>
+  </div>;
+};
+
+export default TodoCard;
+{
+}
