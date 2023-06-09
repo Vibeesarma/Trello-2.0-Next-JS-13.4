c1c070b9e0f7d101a71073e6983cbd095e23edf6
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-06-09 18:57:58 +0100

    image upload function added

--- components/Model.tsx
+++ components/Model.tsx
@@ -1,26 +1,51 @@
 "use client";
 
-import { Fragment } from "react";
+import { FormEvent, Fragment, useRef } from "react";
+import Image from "next/image";
 import { Dialog, Transition } from "@headlessui/react";
 import { useModelStore } from "@/store/ModelStore";
 import { useBoardStore } from "@/store/BoardStore";
 import TaskTypeRadioGroup from "./TaskTypeRadioGroup";
+import { PhotoIcon } from "@heroicons/react/24/solid";
 
 function Model() {
-  const [newTaskInput, setNewTaskInput] = useBoardStore((state) => [
-    state.newTaskInput,
-    state.setNewTaskInput,
-  ]);
+  //  this use  for when click the ui element for the image upload this ref will trigger the hidden file input
+  const imagePickerRef = useRef<HTMLInputElement>(null);
+
+  const [newTaskInput, setNewTaskInput, setImage, image, addTask, newTaskType] =
+    useBoardStore((state) => [
+      state.newTaskInput,
+      state.setNewTaskInput,
+      state.setImage,
+      state.image,
+      state.addTask,
+      state.newTaskType,
+    ]);
 
   const [isOpen, closeModel] = useModelStore((state) => [
     state.isOpen,
     state.closeModel,
   ]);
 
+  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!newTaskInput) return;
+
+    // add task
+    addTask(newTaskInput, newTaskType, image);
+    setImage(null);
+    closeModel();
+  };
+
   return (
     // Use the `Transition` component at the root level
     <Transition appear show={isOpen} as={Fragment}>
-      <Dialog as="form" className="relative z-10" onClose={closeModel}>
+      <Dialog
+        as="form"
+        onSubmit={handleSubmit}
+        className="relative z-10"
+        onClose={closeModel}
+      >
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
@@ -63,6 +88,51 @@ function Model() {
 
                 {/* radio group */}
                 <TaskTypeRadioGroup />
+
+                <div>
+                  <button
+                    type="button"
+                    onClick={() => imagePickerRef.current?.click()}
+                    className="w-full border border-gray-300 rounded-md outline-none p-5 focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
+                  >
+                    <PhotoIcon className="h-6 w-6 mr-2 inline-block" />
+                    Upload Image
+                  </button>
+
+                  {image && (
+                    <Image
+                      src={URL.createObjectURL(image)}
+                      alt="upload image"
+                      width={200}
+                      height={200}
+                      className="w-full h-44 object-cover mt-2 filter hover:grayscale transition-all duration-150 cursor-not-allowed"
+                      onClick={() => setImage(null)}
+                    />
+                  )}
+
+                  <input
+                    type="file"
+                    ref={imagePickerRef}
+                    hidden
+                    onChange={(e) => {
+                      // check image
+                      if (!e.target.files![0].type.startsWith("image/")) {
+                        return;
+                      }
+
+                      setImage(e.target.files![0]);
+                    }}
+                  />
+                </div>
+                <div className="mt-2">
+                  <button
+                    type="submit"
+                    disabled={!newTaskInput}
+                    className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2 disabled:bg-gray-100 disabled:text-gray-300 disabled:cursor-not-allowed"
+                  >
+                    Add Task
+                  </button>
+                </div>
               </Dialog.Panel>
             </Transition.Child>
           </div>
--- store/BoardStore.ts
+++ store/BoardStore.ts
@@ -1,5 +1,6 @@
-import { databases, storage } from "@/appwrite";
+import { ID, databases, storage } from "@/appwrite";
 import { getTodosGroupedByColumn } from "@/lib/getTodosGroupedByColumn";
+import uploadImage from "@/lib/uploadImage";
 import { create } from "zustand";
 
 interface BoardState {
@@ -10,10 +11,13 @@ interface BoardState {
   searchString: string;
   newTaskInput: string;
   newTaskType: TypedColumn;
+  image: File | null;
   setNewTaskInput: (input: string) => void;
   setNewTaskType: (columnId: TypedColumn) => void;
+  addTask: (todo: string, columnId: TypedColumn, image?: File | null) => void;
   deleteTask: (taskIndex: number, todo: Todo, id: TypedColumn) => void;
   setSearchString: (searchString: string) => void;
+  setImage: (image: File | null) => void;
 }
 
 const useBoardStore = create<BoardState>((set, get) => ({
@@ -22,6 +26,7 @@ const useBoardStore = create<BoardState>((set, get) => ({
   },
   searchString: "",
   newTaskInput: "",
+  image: null,
   newTaskType: "todo",
   setSearchString: (searchString) => set({ searchString }),
   getBoard: async () => {
@@ -57,6 +62,63 @@ const useBoardStore = create<BoardState>((set, get) => ({
   },
   setNewTaskInput: (input) => set({ newTaskInput: input }),
   setNewTaskType: (columnId) => set({ newTaskType: columnId }),
+  setImage: (image) => set({ image }),
+
+  addTask: async (todo, columnId, image) => {
+    let file: Image | undefined;
+    if (image) {
+      const fileUploaded = await uploadImage(image);
+      if (fileUploaded) {
+        file = {
+          bucketId: fileUploaded.bucketId,
+          fileId: fileUploaded.$id,
+        };
+      }
+    }
+
+    const { $id } = await databases.createDocument(
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_TODOS_COLLECTION_ID!,
+      ID.unique(),
+      {
+        title: todo,
+        status: columnId,
+        ...(file && { image: JSON.stringify(file) }),
+      }
+    );
+
+    set({ newTaskInput: "" });
+
+    set((state) => {
+      const newColumns = new Map(state.board.columns);
+      console.log("🚀 ~ file: BoardStore.ts:94 ~ set ~ newColumns:", newColumns)
+
+      const newTodo: Todo = {
+        $id,
+        $createdAt: new Date().toISOString(),
+        title: todo,
+        status: columnId,
+        ...(file && { image: file }),
+      };
+
+      const column = newColumns.get(columnId);
+
+      if (!column) {
+        newColumns.set(columnId, {
+          id: columnId,
+          todos: [newTodo],
+        });
+      } else {
+        newColumns.get(columnId)?.todos.push(newTodo);
+      }
+
+      return {
+        board: {
+          columns: newColumns,
+        },
+      };
+    });
+  },
 }));
 
 export { useBoardStore };
