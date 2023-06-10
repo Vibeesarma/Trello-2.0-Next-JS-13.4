2f15af685afb34a90839cc0b5b35d349f9780ecd
Vibeesarma <77588716+Vibeesarma@users.noreply.github.com>
2023-06-10 06:22:19 +0100

    temp data for open ai

--- lib/fetchSuggestion.ts
+++ lib/fetchSuggestion.ts
@@ -3,18 +3,31 @@ import formatTodoForAI from "./formatTodoForAI";
 const fetchSuggestion = async (board: Board) => {
   const todos = formatTodoForAI(board);
 
-  console.log("fetch Todo", todos);
+  //   this is get from ai respone -------------------
+  //   const res = await fetch("/api/generateSummary", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify({ todos }),
+  //   });
 
-  const res = await fetch("/api/generateSummary", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ todos }),
+  //   const GPTdata = await res.json();
+  //   const { content } = GPTdata;
+
+  // tempory response for limited for open ai---------------------------
+
+  const pro = new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("hello");
+    }, 3000);
   });
+  await pro;
 
-  const GPTdata = await res.json();
-  const { content } = GPTdata;
+  const content =
+    "Welcome Mr. Vibeesarma! There are 1 to-do, 1 in-progress, and 1 done task in the list. Have a productive day!";
+
+  // ------------
 
   return content;
 };
